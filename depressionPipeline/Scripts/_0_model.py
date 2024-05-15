651873b0a4b228b9b1fd2938e47086d7f979fca6
paulinosalmon <paulinosalmon@gmail.com>
2024-05-15 19:45:48 +0800

    Fix wrong commit from LSTM to GRU

--- depressionPipeline/Scripts/_0_model.py
+++ depressionPipeline/Scripts/_0_model.py
@@ -8,11 +8,11 @@ from settings import channelNames, channelNamesExcluded
 # Define the function to create the RNN model
 def create_eeg_model(input_shape=(23, 110, 1), num_classes=2):
     model = Sequential([
-        # Reshape input to match the format required for LSTM
+        # Reshape input to match the format required for GRU
         Reshape((input_shape[0] * input_shape[1], input_shape[2]), input_shape=input_shape),  # Shape: (None, 2530, 1)
         
-        # LSTM layer
-        LSTM(128, return_sequences=True),  # Shape: (None, 2530, 128)
+        # GRU layer
+        GRU(128, return_sequences=True),  # Shape: (None, 2530, 128)
         
         # Flatten layer
         Flatten(),  # Shape: (None, 323840)
