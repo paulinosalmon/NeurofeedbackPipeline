689a03634024e89ee36411e3b144ed9bde648e6a
paulinosalmon <paulinosalmon@gmail.com>
2024-05-17 00:28:36 +0800

    Update format of model for transformers

--- depressionPipeline/Scripts/_0_model.py
+++ depressionPipeline/Scripts/_0_model.py
@@ -1,31 +1,31 @@
-# Separate script for the deep learning model for easier modularity
-
-import tensorflow as tf
 import math
-from tensorflow.keras.models import Sequential
-from keras.models import Sequential
-from keras.layers import LSTM, Dense, Dropout, Flatten, Reshape, SimpleRNN, GRU, Bidirectional
-from settings import channelNames, channelNamesExcluded
+import tensorflow as tf
+from tensorflow.keras.layers import (Dense, Flatten, Dropout, Reshape, GRU, Input, 
+                                    LayerNormalization, MultiHeadAttention, Add, Activation)
+from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras.optimizers import Adam                
 
-# Define the function to create the RNN model
 def create_eeg_model(input_shape=(23, 110, 1), num_classes=2):
-    model = Sequential([
-        # Reshape input to match the format required for GRU
-        Reshape((input_shape[0] * input_shape[1], input_shape[2]), input_shape=input_shape),  # Shape: (None, 2530, 1)
-        
-        # GRU layer
-        GRU(128, return_sequences=True),  # Shape: (None, 2530, 128)
-        
-        # Flatten layer
-        Flatten(),  # Shape: (None, 323840)
-        
-        # Dense layer
-        Dense(64, activation='relu', kernel_regularizer='l2'),  # Shape: (None, 64)
-        Dropout(0.5),
-        
-        # Output layer for binary classification
-        Dense(1, activation='sigmoid')  # Shape: (None, 1)
-    ])
+    inputs = Input(shape=input_shape)
+    
+    # Reshape input to match the format required for GRU
+    x = Reshape((input_shape[0] * input_shape[1], input_shape[2]))(inputs)  # Shape: (None, 2530, 1)
+    
+    # GRU layer
+    x = GRU(128, return_sequences=True)(x)  # Shape: (None, 2530, 128)
+    
+    # Flatten layer
+    x = Flatten()(x)  # Shape: (None, 323840)
+    
+    # Dense layer
+    x = Dense(64, activation='relu', kernel_regularizer='l2')(x)  # Shape: (None, 64)
+    x = Dropout(0.5)(x)
+    
+    # Output layer for binary classification
+    outputs = Dense(1, activation='sigmoid')(x)  # Shape: (None, 1)
+    
+    model = Model(inputs=inputs, outputs=outputs)
+    
     return model
     
 def scheduler(epoch, lr):
